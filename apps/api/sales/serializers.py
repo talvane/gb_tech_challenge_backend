00ce36ea9d99e710e4f7dff63d53aec09870c909
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-27 12:35:21 -0300

    change serializer with fields and validation

--- apps/api/sales/serializers.py
+++ apps/api/sales/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 from apps.sales.models import Sale
 
@@ -6,9 +7,17 @@ from apps.sales.models import Sale
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
-        exclude = [
-            'id',
-            'created_at',
-            'updated_at'
+        fields = [
+            'code',
+            'value',
+            'date',
+            'cpf',
+            'status',
+            'description_status'
         ]
         ready_only_fields = ('status',)
+
+    def validate_value(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(_('Value invalid.'))
+        return value
