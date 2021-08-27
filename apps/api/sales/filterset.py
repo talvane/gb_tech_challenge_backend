80f7fb54d0cd886cf363480744af4e9a3cdc41f3
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-27 13:56:23 -0300

    feat add filterset for sale

---
+++ apps/api/sales/filterset.py
@@ -0,0 +1,31 @@
+from django_filters.filters import ChoiceFilter, DateFromToRangeFilter
+from django.utils.translation import ugettext as _
+
+from apps.utils.filterset import SearchFilterSet
+from apps.sales.models import Sale
+
+
+FILTER_CHOICES = [
+    ('IN', _('In Validation')),
+    ('AP', _('Approved')),
+]
+
+
+class SaleFilter(SearchFilterSet):
+    date = DateFromToRangeFilter()
+    filter_by = ChoiceFilter(choices=FILTER_CHOICES, method='filter_by_value')
+
+    class Meta:
+        model = Sale
+        fields = ['q', 'date', 'filter_by']
+        search_fields = [
+            'code__icontains',
+            'cpf__cpf__icontains'
+        ]
+
+    def filter_by_value(self, queryset, name, value):
+        if(value == 'IN'):
+            return queryset.filter(status='IN')
+        elif(value == 'AP'):
+            return queryset.filter(status='AP')
+        return queryset
--- apps/api/sales/views.py
+++ apps/api/sales/views.py
@@ -3,9 +3,11 @@ from django_filters import rest_framework as filters
 
 from apps.sales.models import Sale
 from .serializers import SaleSerializer
+from .filterset import SaleFilter
 
 
 class SaleViewSet(viewsets.ModelViewSet):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
     filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SaleFilter
